7a40007d0fa4ba2fbf41050d903d191b60081b5e
ATI <ATI@ATI-PC>
2023-03-08 19:14:15 -0300

    Criação do projeto

---
+++ Teste.Erick/src/Erick.java
@@ -0,0 +1,15 @@
+
+public class Erick {
+	
+	/*Main é um método auto executável*/
+	public static void main(String[] args) {
+		
+		/*Escreve texto no console*/
+		System.out.println("Primeiro programa em Java");
+		
+		
+	}
+	
+	
+
+}
